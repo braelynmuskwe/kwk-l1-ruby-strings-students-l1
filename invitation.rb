681af45145ad5cdff55dc59f2dd15a04ff910b8a
braelynmuskwe <braemira1@icloud.com>
2018-07-08 17:36:21 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -6,5 +6,6 @@ def invitation
  puts "You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30."
  puts "Sincerely,"
 puts "Harry Potter"
+"#{name}=Braelyn"
 end 
 invitation
