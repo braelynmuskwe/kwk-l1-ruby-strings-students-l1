c49767881652a7b725ae0c7cf8cd8d7a84dad9c3
braelynmuskwe <braemira1@icloud.com>
2018-07-08 17:37:37 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -2,7 +2,7 @@
 
 # Try starting out with puts'ing a string.
 def invitation
-  puts "Dear Hermione",
+  puts "Dear Hermione,"
  puts "You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30."
  puts "Sincerely,"
 puts "Harry Potter"
